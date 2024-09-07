31ca8c0b5587cfda79be273bade95919e1802d41
Shoaib Ghulam <shoaibghulam@hnhtechsolutions.com>
2024-09-08 00:41:40 +0500

    shaoib mn about page a backgrond color change ko

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export default function About() {
   return (
     <div>
-        <h1>About Me</h1>
+        <h1 style={{backgroundColor:"#333", color:"#ffffff"}}>About Me</h1>
         <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil obcaecati exercitationem enim deleniti nulla voluptatum magni, laborum vitae adipisci facere optio doloribus ipsum quos reprehenderit amet inventore alias repudiandae a?
         </p>
