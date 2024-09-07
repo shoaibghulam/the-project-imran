863035dbc72b744ad4407adbdd749e2de6bfa96f
Shoaib Ghulam <shoaibghulam@hnhtechsolutions.com>
2024-09-08 00:38:22 +0500

    noke change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+        <h1>About Me</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil obcaecati exercitationem enim deleniti nulla voluptatum magni, laborum vitae adipisci facere optio doloribus ipsum quos reprehenderit amet inventore alias repudiandae a?
+        </p>
+    </div>
   )
 }
